5edd837c7a52ddbd9720b0527bbe9f8f2d97bce9
kiyohara@re-feed.jp <kiyohara@re-feed.jp>
2021-10-28 17:41:44 +0900

    ファイル直下に出力するように変更 ファイルも自動生成する

--- diff_archive.sh
+++ diff_archive.sh
@@ -2,6 +2,10 @@ function git_diff_archive {
 	# ディレクトリの移動
 	echo "$2の差分出力を行います。"
 	cd $2
+	create_dir=`echo "$(pwd)" | sed -e 's/.*\/\([^\/]*\)$/\1/'`
+	echo $create_dir
+	create_dir="${create_dir}_$(date "+%Y%m%d_%H-%M-%S")"
+	mkdir $dir_name/diff_file/$create_dir
 	# 変数定義
 	local diff=""
 	local h="HEAD"
@@ -10,17 +14,21 @@ function git_diff_archive {
 	if [ $# -eq 3 ]; then
 		if expr "$3" : '[0-9]*$' >/dev/null; then
 			diff="HEAD~${3} HEAD"
+			file_name="HEAD"
 		else
 			diff="${3} HEAD"
+			file_name=$3
 		fi
 	elif [ $# -eq 4 ]; then
 		diff="${4} ${3}"
 		h=$3
+		file_name=$4
 	fi
 	if [ "$diff" != "" ]; then
 		diff="git diff --diff-filter=d --name-only ${diff}"
 	fi
-	git archive --format=zip --prefix=new/ $h `eval $diff` -o $dir_name/new.zip
+	echo "${diff}の差分を出力しています。"
+	git archive --format=zip --prefix=$file_name/ $h `eval $diff` -o $dir_name/diff_file/$create_dir/$file_name.zip
 	branch_all=$(git branch --contains ${3})
 	branch=`echo $branch_all | cut --delim=" " -f 1`
 	git checkout $branch
@@ -29,17 +37,21 @@ function git_diff_archive {
 	if [ $# -eq 3 ]; then
 		if expr "$3" : '[0-9]*$' >/dev/null; then
 			diff="HEAD HEAD~${3}"
+			file_name="HEAD"
 		else
 			diff="HEAD ${3}"
+			file_name=$3
 		fi
 	elif [ $# -eq 4 ]; then
 		diff="${3} ${4}"
 		h=$4
+		file_name=$3
 	fi
 	if [ "$diff" != "" ]; then
 		diff="git diff --diff-filter=d --name-only ${diff}"
 	fi
-	git archive --format=zip --prefix=old/ $h `eval $diff` -o $dir_name/old.zip
+	echo "${diff}の差分を出力しています。"
+	git archive --format=zip --prefix=$file_name/ $h `eval $diff` -o $dir_name/diff_file/$create_dir/$file_name.zip
 }
 # この関数でディレクトリ名を取得したい
 function get_dir_name {
